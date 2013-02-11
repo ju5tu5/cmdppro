7f35d440ff452d5713077b2d93d084c07281bc0f
Justus Sturkenboom <justus@ju5tu5.nl>
2013-02-11 13:10:37 +0100

    After lecture 1

--- static/js/skeleton.js
+++ static/js/skeleton.js
@@ -1,6 +1,6 @@
 /* Copyleft 2013, all wrongs reversed */
 /*jslint browser: true*/
-/*globals console,prompt*/
+/*globals console*/
 
 (function () {
   'use strict';
@@ -17,21 +17,9 @@
   // ------------------------------
 
 
-  // var naam = prompt("Hoe heet jij?");
 
-  // if (naam === "Justus") {
-  //   console.log("Whoaaa, de docent heeft ingelogd...");
-  // }
 
-  var budget = prompt("Hoeveel geld heb je bij je?");
 
-  if (budget > 24) {
-    console.log("Wow, jij bent rijk!");
-  } else if (budget < 3) {
-    console.log("Dat wordt lunch bietsen...");
-  } else {
-    console.log("Goh dan heb jij het gemiddeld genomen wel oke...");
-  }
 
 
   // ------------------------------
