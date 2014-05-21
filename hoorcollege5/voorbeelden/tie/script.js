918cfaf89ad56e357c855884d196fc378fe22e8d
Justus Sturkenboom <justus@ju5tu5.nl>
2014-05-21 13:46:42 +0200

    College 5

---
+++ hoorcollege5/voorbeelden/tie/script.js
@@ -0,0 +1,8 @@
+/* Copyleft 2013, all wrongs reversed */
+/*jslint browser: true, vars: true*/
+/*globals console,confirm,prompt,alert,document,window*/
+
+window.onload = function () {
+    'use strict';
+
+};
